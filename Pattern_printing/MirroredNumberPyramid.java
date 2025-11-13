84fe4c6afb76c93eecec674645a58b5da39131e0
vikramsolanki12 <solankijogaram5@gmail.com>
2025-11-13 23:58:10 +0530

    initial commit

---
+++ Pattern_printing/MirroredNumberPyramid.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MirroredNumberPyramid {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int n = scn.nextInt();
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(j + " ");
+            }
+            for (int j = n - i - 1; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
---
+++ Pattern_printing/NumberSpiral.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class NumberSpiral {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int n = scn.nextInt();
+
+        for(int i = 1 ; i <= 2*n-1 ; i++){
+            for(int j =1 ; j <= 2*n-1 ; j++){
+                int a = i , b = j;
+                if(i>n) a=2*n-i;
+                if(j>n) b=2*n-j;
+                System.out.print(n+1-Math.min(a,b)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
