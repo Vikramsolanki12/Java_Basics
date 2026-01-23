3e51a1c0599f2ed4fb6f5e9672bacca10d373e0a
vikramsolanki12 <solankijogaram5@gmail.com>
2026-01-23 22:14:15 +0530

    initial commit

---
+++ sorting/src/decreasingSort.java
@@ -0,0 +1,23 @@
+public class decreasingSort {
+    public static void main(String[] args) {
+        int[] arr = {3,1,67,34,23,87,18};
+        int n = arr.length;
+        for( int ele : arr){
+            System.out.print(ele+" ");
+        }
+
+        for(int i=0 ; i<n-1 ; i++){
+            for(int j=0 ; j<n-1-i ; j++){
+                if(arr[j]<arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println();
+        for( int ele : arr){
+            System.out.print(ele+" ");
+        }
+    }
+}
