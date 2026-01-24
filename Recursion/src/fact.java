0ea51c256de8cde94b9aca1bb3fa240d58ebb049
vikramsolanki12 <solankijogaram5@gmail.com>
2026-01-24 21:32:58 +0530

    initial commit

---
+++ Recursion/src/fact.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class fact {
+    public static int fact(int n){
+        if(n==0 || n==1) return 1;
+        return n*fact(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        System.out.println(fact(n));
+    }
+}
